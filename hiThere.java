7c39267b55f8f505a66d4cafa246fc269b565723
santhoshncdevops <ncforuse7@gmail.com>
2018-07-14 21:39:45 +0530

    initial commit 4

--- hiThere.java
+++ hiThere.java
@@ -3,6 +3,6 @@ public class hiThere
  public static void main(String[] args)
 {
 System.out.println("Web Hooks");
-System.out.println("Web Hooks demo 123456 789466");
+System.out.println("Web Hooks demo 123456");
 }
 }
